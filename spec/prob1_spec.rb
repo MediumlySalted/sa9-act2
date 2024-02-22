156b6007828bb0179a09a9ed0adfc70f1d9895b1
Gabriel Adkins <gabea38135@yahoo.com>
2024-02-22 17:30:02 -0600

    rspec setup + Problem 1 completion

---
+++ spec/prob1_spec.rb
@@ -0,0 +1,26 @@
+require 'prob1'
+
+RSpec.describe User do
+  describe "#log_in" do
+    it "logs the user in" do
+      user = User.new('name')
+      user.log_in
+      expect(user.logged_in).to be true
+    end
+  end
+
+  describe "#log_out" do
+    it "logs the user out" do
+      user = User.new('name')
+      user.log_out
+      expect(user.logged_in).to be false
+    end
+  end
+
+  describe "#username" do
+    it "returns the correct username" do
+      user = User.new('testname')
+      expect(user.username).to eq('testname')
+    end
+  end
+end
